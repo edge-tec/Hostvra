b5fd662ccda6aa9575a4354e687d37b64f6777ef
Mizanur Rahman <mizanurrahmanedge@outlook.com>
2026-09-08 02:34:54 +0600

    feat(deploy): include hostvra-api in PM2 ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   apps: [
     {
+      name: 'hostvra-api',
+      script: './bin/hostvra-api',
+      cwd: './',
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: '500M',
+      env: {
+        PORT: 8080,
+      },
+    },
+    {
       name: 'hostvra-web',
       script: 'npm',
       args: 'run start',
@@ -15,3 +26,4 @@ module.exports = {
     },
   ],
 };
+
